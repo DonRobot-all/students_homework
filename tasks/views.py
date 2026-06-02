b60989c06bc6064598f56b6983a9f20547517858
Ilya <pankovskiy.im@gmail.com>
2026-06-02 14:36:32 +0300

    admin+model

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Task
+
 # Create your views here.
 
 def index(request):
@@ -11,22 +13,26 @@ def task_list(request):
     # return HttpResponse("Привет! Это список заданий")
     # return render(request, 'tasks/task_list.html')
     # tasks = ['Сделать уроки', 'Прочитать книгу', 'Помыть посуду']
-    tasks = [
-        {'title': 'Сделать уроки',    'description': 'Математика и русский'},
-        {'title': 'Прочитать книгу',  'description': 'Глава 3 и 4'},
-        {'title': 'Написать код',    'description': 'Django проект'},
-    ]
+    # tasks = [
+    #     {'title': 'Сделать уроки',    'description': 'Математика и русский'},
+    #     {'title': 'Прочитать книгу',  'description': 'Глава 3 и 4'},
+    #     {'title': 'Написать код',    'description': 'Django проект'},
+    # ]
+    # return render(request, 'tasks/task_list.html', {'tasks': tasks})
+    tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
-
+    
 def news(request, pk):
     return HttpResponse(f"Проверка страница {pk}")
 
 
 def task_detail(request, pk):
-    tasks = [
-        {'title': 'Сделать уроки',    'description': 'Математика и русский'},
-        {'title': 'Прочитать книгу',  'description': 'Глава 3 и 4'},
-        {'title': 'Написать код',    'description': 'Django проект'},
-    ]
-    task = tasks[pk - 1]   # pk с 1, список с 0
+    # tasks = [
+    #     {'title': 'Сделать уроки',    'description': 'Математика и русский'},
+    #     {'title': 'Прочитать книгу',  'description': 'Глава 3 и 4'},
+    #     {'title': 'Написать код',    'description': 'Django проект'},
+    # ]
+    # task = tasks[pk - 1]   # pk с 1, список с 0
+    # return render(request, 'tasks/task_detail.html', {'task': task, 'pk': pk})
+    task = Task.objects.get(pk=pk)
     return render(request, 'tasks/task_detail.html', {'task': task, 'pk': pk})
